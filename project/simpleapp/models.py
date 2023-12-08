2360381204a4415506647990282fb3e1e11a0482
DS-975 <975ds579@gmail.com>
2023-12-08 15:46:14 +0300

    (master)_Создали_модели_Product|Category(D3.2)_(08.12.2023_16:44:00)

--- project/simpleapp/models.py
+++ project/simpleapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+# Товар нашей витрины
+class Product(models.Model):
+    name = models.CharField( # models - единствинный и точный источник информации в ваших данныхю ОН содержит основные поля и поведения данных, которые вы храните. Обычно каждая модель сопостовляеться с одной таблицей базы данных
+                             # CharField ???
+        max_length=50, # Длина 50 символов
+        unique=True, # Название товаров не должно повторяться
+    )
+
+    description = models.TextField() # TextField ???
+
+    quantity = models.IntegerField( # IntegerField ???
+        validators=[MinValueValidator(0)],
+    )
+
+    # Поле категории будет ссылаться на модель категории
+    category = models.ForeignKey(
+        to='Category',
+        on_delete=models.CASCADE,
+        related_name='products' # Все продукты в категории будут доступны через поле products
+    )
+
+    price = models.FloatField(
+        validators=[MinValueValidator(0.0)],
+    )
+
+    def __str__(self):
+        return f'{self.name.title()}:{self.description[:20]}'
+
+# Категория, которой будет привязывать товар
+class Caterory(models.Model):
+    # Название категории тоже не должны повторяться
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name.title()
